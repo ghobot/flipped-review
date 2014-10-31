01973ff822f31a72221eb6e262daead938366e2f
ghobot <greg@sciencelifeny.com>
2014-10-31 17:31:27 -0400

    make style and interaction changes with popcorn

--- assets/js/review.js
+++ assets/js/review.js
@@ -1,57 +1,68 @@
 $( document ).ready(function() {
     //in points array
-    var inPoints = new Array();
-    
-
-    //create popcorn instance
-    
-    
-    
-    //find all in-points in table and make array
+    //var inPoints = new Array();
+    var framerate = 23.98;
    
-    //getInPoints();
-   
-   var pop = Popcorn("#main");
-
+     //create popcorn instance  
+   var pop = Popcorn("#main", { 
+        "frameAnimation": true,      
+        "framerate": framerate 
+        });
+    popcornVideoFootnotes(pop);
     pop.play();
+   
+    
     $('#metadata').removeClass('hide');
     
-var ready = pop.readyState();    
+    var ready = pop.readyState();    
     if (ready==4){
-        
-    
     }
     
    if ($(".header").not()) {
-       $("tr:not(.header)").click(function() {
-           var time = $('td:eq(3)',this).html();
-           console.log("time: " + time);
-           // var type = jQuery.type(time);
-           //console.log("type: " + type);
-           var time2 = time.split(/:(.+)?/)[1];
-           pop.currentTime("00:17:08");
-            console.log("time2: " + time2);
-        });
-    
+        $("tr:not(.header)").click(function() {
+            var time = $('td:eq(3)',this).text();
+            var timeInSeconds = convertSeconds(time,framerate);
+            pop.currentTime(timeInSeconds);
+        });   
    }
-function popcornVideo (){
-   
-   
-}
-    
-    
-function getInPoints(){
-    
-    //Iterate all td's in second column
-    $('#metadata tbody tr td:nth-child(4)').each( function(){
-       //add item to array
-       inPoints.push( $(this).text() );       
-    });
-   console.log("inpoints: " + inPoints);
-}    
 
-// listen for click events
+function popcornVideoFootnotes(popcorn){ //for every row, create a popcorn footnote
     
+   $('#metadata tbody tr').each(function(){    
+            var start = $('td:nth-child(4)', this).html(),
+                end = $('td:nth-child(5)', this).html(),
+                question = $('td:nth-child(3) pre', this).html(),
+                target = $('#question span');
+
+            //console.log(start+", "+end+", "+question);
+
+            popcorn.footnote({
+                start: convertSeconds(start, framerate),
+                end: convertSeconds(end, framerate),
+                text: question,
+                target: "target"        
+            });  
+})
+//console.log("footnotes created");   
+}
+
+function convertSeconds(time,framerate) {
+    var array = time.split(':');
+    var frames = Number(array[3]) , seconds = Number(array[2]) , minutes = Number(array[1]),  hours = Number(array[0]);  
+    var convertedSeconds = frames/framerate + seconds + 60*minutes + 3600*hours;    
+    return convertedSeconds;
+}    
+    
+    
+//function getInPoints(){
 //    
+//    //Iterate all td's in second column
+//    $('#metadata tbody tr td:nth-child(4)').each( function(){
+//       //add item to array
+//       inPoints.push( $(this).text() );       
+//    });
+//   console.log("inpoints: " + inPoints);
+//}    
+
     
-});
+})
